2b73408f1624178e6fd8230b4c8f91157d697b94
salah <soutliou@gmail.com>
2023-05-14 17:06:40 +0100

    Initial commit

---
+++ src/main/java/com/example/jabaklahprojectebankbackend/JabakLahProjectEBankBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.jabaklahprojectebankbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JabakLahProjectEBankBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JabakLahProjectEBankBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jabaklahprojectebankbackend/JabakLahProjectEBankBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jabaklahprojectebankbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JabakLahProjectEBankBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
