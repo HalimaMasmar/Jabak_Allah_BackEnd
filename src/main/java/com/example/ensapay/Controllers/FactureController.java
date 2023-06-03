1b94a9549c9c8cbdaaad7265ef045877e9ada4dd
salah <soutliou@gmail.com>
2023-06-03 18:54:35 +0100

    second commit

---
+++ src/main/java/com/example/ensapay/Controllers/FactureController.java
@@ -0,0 +1,77 @@
+package com.example.ensapay.Controllers;
+
+
+import com.example.ensapay.models.Facture;
+import com.example.ensapay.request.CreateFactureRequest;
+import com.example.ensapay.request.FactureRequest;
+import com.example.ensapay.request.RechargeRequest;
+import com.example.ensapay.request.SignupRequestClient;
+import com.example.ensapay.service.FactureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.mail.MessagingException;
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.List;
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/facture")
+public class FactureController {
+
+    @Autowired
+    private FactureService factureService;
+
+    @PostMapping("/payerFacture")
+
+    public Boolean payerFacture(@Valid @RequestBody FactureRequest factureRequest) throws IOException {
+        factureService.PayerFacture(factureRequest.getRef(),factureRequest.getOwnerphone());
+        return true;
+    }
+
+    @PostMapping("/payerRecharge")
+
+    public Boolean payerRecharge(@Valid @RequestBody RechargeRequest rechargeRequest) throws IOException {
+        factureService.PayerRecharge(rechargeRequest.getValue(),rechargeRequest.getOwnerphone());
+        return true;
+    }
+
+    @PostMapping("/listFacturecreancier")
+
+    public ResponseEntity<List<Facture>>  getFacturesCreancier(@Valid @RequestBody FactureRequest factureRequest) {
+
+        return ResponseEntity.ok().body(factureService.getListFacturesOfCreancier(factureRequest.getCreancier(),factureRequest.getOwnerphone()));
+
+    }
+
+    @GetMapping("/listFacturecreancier/{creancier}/{ownerphone}")
+
+    public ResponseEntity<List<Facture>>  getFacturesCreancier2(@Valid @PathVariable String creancier,@PathVariable String ownerphone) {
+
+        return ResponseEntity.ok().body(factureService.getListFacturesOfCreancier(creancier, ownerphone));
+
+    }
+    @GetMapping("/listFacture")
+    @PreAuthorize("hasRole('CLIENT') ")
+    public List<Facture> getFactureList(@Valid @RequestBody FactureRequest factureRequest){
+        return factureService.getListFactures(factureRequest.getOwnerphone());
+    }
+
+    @GetMapping("/listFacturePaid/{ownerphone}")
+    public List<Facture> getFactureListPaid(@Valid @PathVariable String ownerphone){
+        return factureService.getListFacturePaid(ownerphone);
+    }
+
+    @PostMapping("/addFacture")
+    public ResponseEntity<?> registerFacture(@Valid @RequestBody CreateFactureRequest createFactureRequest) throws IOException {
+
+        factureService.createFacture(createFactureRequest.getStatus(),createFactureRequest.getMontant_fac(),createFactureRequest.getDescription(),
+                createFactureRequest.getDate_emission(),createFactureRequest.getCreancier(),createFactureRequest.getOwnerphone());
+
+        return ResponseEntity.ok("User registered successfully!");
+    }
+
+
+}
---
+++ src/main/java/com/example/ensapay/models/Facture.java
@@ -0,0 +1,41 @@
+package com.example.ensapay.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Entity
+public class Facture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id_facture;
+    @Column(name = "ref")
+    private String ref;
+    private String status;
+    private double montant_fac;
+    private String description;
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date date_emission;
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+
+    private Date date_payement;
+    private String creancier;
+    private String ownerphone;
+
+    public Facture(String status, double montant_fac, String description, Date date_emission, String creancier, String ownerphone) {
+
+        this.status = status;
+        this.montant_fac = montant_fac;
+        this.description = description;
+        this.date_emission = date_emission;
+
+        this.creancier = creancier;
+        this.ownerphone = ownerphone;
+    }
+}
